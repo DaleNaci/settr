173fbb9a3c711aac5cb8d01eb7523fa3674f32d8
DaleNaci <dtn22@njit.edu>
2021-08-14 16:31:23 -0400

    General tracking with video example

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-import time
-import datetime
-import argparse
-
-import numpy as np
 import cv2
-import imutils
-from imutils.video import VideoStream
 
 
-vid = cv2.VideoCapture(0)
+vid = cv2.VideoCapture("highway.mp4")
+
+object_detector = cv2.createBackgroundSubtractorMOG2()
 
 while(True):
     ret, frame = vid.read()
 
-    cv2.circle(frame, (50, 50), 30, (0, 0, 255), -1)
+    mask = object_detector.apply(frame)
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 100:
+            cv2.drawContours(frame, [cnt], -1, (0, 255, 0), 2)
+
+    # cv2.circle(frame, (50, 50), 30, (0, 0, 255), -1)
     cv2.imshow("frame", frame)
+    cv2.imshow("mask", mask)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
--- test.py
+++ test.py
@@ -36,3 +36,29 @@ while True:
     if firstFrame is None:
         firstFrame = gray
         continue
+
+	frameDelta = cv2.absdiff(firstFrame, gray)
+	thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+
+	thresh = cv2.dilate(thresh, None, iterations=2)
+	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+
+	for c in cnts:
+		if cv2.contourArea(c) < args["min_area"]:
+			continue
+
+		(x, y, w, h) = cv2.boundingRect(c)
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow("Security Feed", frame)
+	cv2.imshow("Thresh", thresh)
+	cv2.imshow("Frame Delta", frameDelta)
+	key = cv2.waitKey(1) & 0xFF
+
+	if key == ord("q"):
+		break
+
+vs.stop() if args.get("video", None) is None else vs.release()
+cv2.destroyAllWindows()
