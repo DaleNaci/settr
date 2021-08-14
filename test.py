a5983c59d79a9757f09538e23ad6db1ce0dc71cb
DaleNaci <dtn22@njit.edu>
2021-08-14 15:41:32 -0400

    Part of object detection code

---
+++ test.py
@@ -0,0 +1,38 @@
+import argparse
+import datetime
+import time
+
+import cv2
+import imutils
+from imutils.video import VideoStream
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to the video file")
+ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+args = vars(ap.parse_args())
+
+if args.get("video", None) is None:
+    vs = VideoStream(src=0).start()
+    time.sleep(2.0)
+else:
+    vs = cv2.VideoCapture(args["video"])
+
+firstFrame = None
+
+
+while True:
+    frame = vs.read()
+    frame = frame if args.get("video", None) is None else frame[1]
+    text = "Unoccupied"
+
+    if frame is None:
+        break
+
+    frame = imutils.resize(frame, width=500)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    if firstFrame is None:
+        firstFrame = gray
+        continue
